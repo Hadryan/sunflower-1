7ba7a0609c9f6a4a65f885319aaf1808603e2c7c
Julien Beisel <julienbeisel@gmail.com>
2020-12-26 12:07:57 +0100

    :construction: Better BPM detection: Adding a chill mode

--- sunflower/song_analyzer.py
+++ sunflower/song_analyzer.py
@@ -1,6 +1,7 @@
 from .song_loader import Song
 import librosa
 
+
 class SongAnalyzer:
     def __init__(self, song: Song):
         """Creates a SongAnalyzer object.
@@ -25,6 +26,29 @@ class SongAnalyzer:
             raise ValueError("No song was loaded.")
 
         # Detect tempo
-        self.tempo, self.beat_frames = librosa.beat.beat_track(
+        tempo, beat_frames = librosa.beat.beat_track(
             y=self.song.mono_waveform, sr=self.song.sr, tightness=100
         )
+
+        self.tempo = adjust_tempo(tempo)
+        self.beat_frames = beat_frames
+
+
+def adjust_tempo(bpm: float, mode="chill") -> float:
+    """Adjusts the BPM for more coherence (e.g. turning 160 BPM into 80 BPM)
+
+    :param bpm: BPM of the song
+    :param mode: Type of song to analyse.
+    Setting the mode to 'chill' ensures to have a BPM lower to a threshold (110 BPM)
+    """
+
+    THRESOLD = 110
+
+    if mode == "chill":
+
+        while bpm > THRESOLD:
+            bpm = bpm / 2
+
+    bpm = round(bpm, 0)
+
+    return bpm
